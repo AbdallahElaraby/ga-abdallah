f9e847d5f28b5e9d6fefa2d7b271afaba503c5ca
Abdallah Elaraby <pipo.elaraby99@gmail.com>
2025-07-26 13:02:21 +0300

    seq

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -1,2 +1,5 @@
 echo "This is workflow-1 running on main branch from script.sh"
 sleep 30
+sudo apt update -y
+sudo apt install cowsay -y
+cowsay -f dragon "Run for cover, I am a DRAGON....RAWR" >> dragon.txt
